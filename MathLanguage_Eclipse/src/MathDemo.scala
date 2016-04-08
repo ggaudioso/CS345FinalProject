9cc4aa62575b2e8599303cc64295c91dbcbb90eb
Mike Feilbach <mikefeilbach@wireless-10-147-40-25.public.utexas.edu>
2016-04-08 13:33:36 -0500

    Eclipse project for MathLanguage (testing)

---
+++ MathLanguage_Eclipse/src/MathDemo.scala
@@ -0,0 +1,15 @@
+
+object MathDemo extends MathCode{
+  def main(args: Array[String]):Unit = {
+
+    LET('three) BE 3.0
+    LET('four) BE 2 + 2
+    //PRINT('four)
+    //PRINT('newvar)
+    LET('x) BE 'newvar + 'four
+    PRINTLN('x)
+    PRINTLN('x + 2) //eg of what needs to be simplified
+    PRINTLN('x * 2) //eg of need of parenthesis in print sometimes
+ 
+  }
+}
