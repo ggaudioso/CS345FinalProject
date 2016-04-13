d793cb99e46155349b3d1f2f233022aee5c62aa7
Joshie196 <joshdcannon@gmail.com>
2016-04-13 14:34:48 -0700

    added functions to demo

--- MathLanguage_Eclipse/src/main/scala/cs345/MathDemo.scala
+++ MathLanguage_Eclipse/src/main/scala/cs345/MathDemo.scala
@@ -1,26 +1,21 @@
 
 object MathDemo extends MathCode{
   def main(args: Array[String]):Unit = {  
+    'b := 3 + 6
+    
+    'x := 'b
+    
+    'f('x) := 'x + 2
+    
+    'y := 'f('x)
+    'h := 'f(2 + 80)
+    'z := 'f('x + 5)
+    
+    PRINTLN('f)
     
     
-    PRINTLN_APPROXIMATE('pi)
-    PRINTLN_APPROXIMATE('pi+3)
-    PRINTLN_EVALUATE('pi+3)
-    
-    'x := 'pi * 2
-    PRINTLN_APPROXIMATE('x*'r)
-    println
-    PRINTLN_EVALUATE('e+'pi)
-    PRINTLN_APPROXIMATE('e+'pi)
-    println
-    PRINTLN_EVALUATE('e^'pi)
-    PRINTLN_APPROXIMATE('e^'pi)
-    
-    println
-    PRINTLN_EVALUATE(1 / 3)
-    PRINTLN_EVALUATE(1 OVER 3)
-
-    println
-    PRINTLN_EVALUATE((1 OVER 3) - (3 OVER 1))
+    PRINTLN('y)
+    PRINTLN('h)
+    PRINTLN('z)
   }
 }
